7929f8df36adb8d8d6ec871cd5c7d2abf6c2cf78
Paweł <s24803@pjwstk.edu.pl>
2022-06-08 19:09:46 +0200

    join show_products.php

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Cukierki</title>
 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="home.css">
 </head>
@@ -61,9 +62,11 @@
 
     <main>
 
-        <section>
-            Produkty
-        </section>
+        <div class="row">
+            <?php
+                require_once 'show_products.php';
+            ?>
+        </div>
 
     </main>
 
