c7c9959cc034327ea5c47c11f01b8318f4117827
Paweł <s24803@pjwstk.edu.pl>
2022-06-08 19:07:58 +0200

    add system to show products

---
+++ show_products.php
@@ -0,0 +1,27 @@
+<?php
+require_once 'database.php';
+
+$query = "SELECT * FROM produkty";
+
+$result =  $db->query($query);
+
+if (!$result){
+
+    echo "BŁĄD w zapytaniu";
+    exit;
+}
+
+while($row = $result->fetch()){
+
+    echo "<div class='card col-sm-6 col-md-4 col-lg-3'>
+            <img src='img/{$row['obraz']}' class='card-img-top' alt='img/produktu'>
+            <div class='card-body'>
+                <h5 class='card-title'>{$row['nazwa']}</h5>
+                <p class='card-text'>{$row['k_opis']}</p>
+                <p class='card-text'>{$row['cena']} zł</p>
+                <a href='product_page.php?id={$row['id']}' class='btn btn-primary stretched-link'>Zobacz</a>
+            </div>
+          </div>";
+
+}
+?>
