4d3b240f62850467625e2aa3d2711ba39329dac7
Paweł <s24803@pjwstk.edu.pl>
2022-06-16 22:23:08 +0200

    add removing product system

--- a_remove_product.php
+++ a_remove_product.php
@@ -1,2 +1,17 @@
 <?php
+    session_start();
 
+    if($_SESSION['admin_is_logged'] != true){
+
+        header('Location: index.php');
+        exit();
+    }
+
+    require_once 'database.php';
+
+    $removeProduct = $db->prepare('DELETE FROM produkty WHERE id = :id');
+
+    $removeProduct->bindValue(':id',$_POST["id"] );
+    $removeProduct->execute();
+
+    header('Location: admin_page.php');
