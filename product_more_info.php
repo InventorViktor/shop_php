7ea6e1f159f843fdc6f396490948c745d7c0e869
Paweł <s24803@pjwstk.edu.pl>
2022-06-16 23:19:32 +0200

    add php script to show product info

---
+++ product_more_info.php
@@ -0,0 +1,33 @@
+<?php
+
+    //you need $_GET['id']
+    require_once 'database.php';
+
+    $more_info = $db->prepare('SELECT nazwa, cena, d_opis, obraz FROM produkty WHERE id = :id');
+
+    $more_info->bindValue('id', $_GET['id']);
+    $more_info->execute();
+
+    $product_row = $more_info->fetch();
+
+    echo '<p class="fw-bold fs-1">'. $product_row['nazwa'] . '</p>';
+
+    echo
+    "<aside class='col-md-4'>
+            <img class='img-fluid' src='img/{$product_row['obraz']}'  alt='img/produktu'>
+        </aside>";
+
+    echo
+        '<div class="col-md-8">
+            '. $product_row["d_opis"] .'
+        </div>';
+
+    if(isset($_SESSION['is_logged'])){
+
+        echo
+        '<button class="btn btn-outline-success">Dodaj do koszyka</button>';
+    } else {
+
+        echo '<p class="fw-bold">Zaloguj się aby dodać produkt do koszyka</p>';
+    }
+
